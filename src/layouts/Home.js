c14232dd93fc6febdd34f27cc0009ef8563bf545
Burak Kantarcı <burak@thundra.io>
2022-11-16 15:18:01 +0300

    added podcast link

--- src/layouts/Home.js
+++ src/layouts/Home.js
@@ -14,13 +14,14 @@ const Home = () => {
       <div className="mt-36 border-bottom-dash pb-24 ">
         <h2 className="pt-24">What am I working on?</h2>
         <p>My current goal at <a href="https://thundra.io">Thundra</a> as Product Manager is to ease developers’ daily life and make their workflows more errorless and enjoyable.</p>
+        <a className="is-inline mt-8" target="_blank" rel="noopener noreferrer" href="https://youtu.be/ZeNf_gSU0KA">Product Innovation Series - From Designer to Product Manager</a>
         <a className="is-inline mt-8" target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/watch?v=9LoTA0xd-m0">TestJS Summit 2021 - Lightning Talk</a>
         <a className="is-inline mt-8" target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/watch?v=u82DRPbhPYc">Watch me introducing Thundra Sidekick to ProductHunters.</a>
         <a className="is-inline mt-8" target="_blank" rel="noopener noreferrer" href="https://youtu.be/d_GJVcLGA_U?t=1072">Webinar: The Non-Intrusive Debugging on Modern Architectures</a>
       </div>
       <div className="mt-36 border-bottom-dash pb-24">
         <h2>Past Projects</h2>
-        <p>I have 5+ years of experience in shipping digital products. <Link to="/projects">See the list of the things I've ever made and contributed.</Link></p>
+        <p>I have 7+ years of experience in shipping digital products. <Link to="/projects">See the list of the things I've ever made and contributed.</Link></p>
       </div>
       <div className="mt-36">
         <h2>Get in Touch</h2>
